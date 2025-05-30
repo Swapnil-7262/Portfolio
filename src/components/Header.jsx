0d4353cf4bcf29f88795558d02251f1333871e98
Swapnil-7262 <chaudharis7262@gmail.com>
2025-05-30 19:52:46 +0530

    Fix logo image path in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center">
         <a href="/" >
           <div className="flex justify-center items-center gap-2">
-            <img className="w-12 md:w-20 xl:w-28  " src="/public/Images/logo.png" alt="logo.png" />
+            <img className="w-12 md:w-20 xl:w-28  " src="/Images/logo.png" alt="logo.png" />
             <p className="text-4xl font-semibold capitalize cursor-pointer">Swapnil<span className="text-accent">.</span></p>
           </div>
         </a>
